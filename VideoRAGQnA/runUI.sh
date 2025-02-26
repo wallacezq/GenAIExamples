57bca2e38e1fa8ce864f215458242f565243ab13
wallacezq <wallace.lzq@gmail.com>
2025-02-27 00:48:35 +0800

    fix runUI.sh script error

--- VideoRAGQnA/runUI.sh
+++ VideoRAGQnA/runUI.sh
@@ -2,4 +2,4 @@
 
 # Run the Python script for UI
 #streamlit run video-rag-ui.py docs/config.yaml --server.address 0.0.0.0 --server.port 50055
-streamlit run video-rag-ui.py docs/config.yaml --server.address 127.0.0.1 --server.port 50022
+streamlit run video-rag-ui.py  --server.address 127.0.0.1 --server.port 50022
